5c432c69cff746b10ae523a89a7fa70749aa2044
Serhiy Mytrovtsiy <mitrovtsiy@ukr.net>
2019-10-25 23:06:26 +0200

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -7,20 +7,20 @@ if [ ! -e "$HOME/.docker/config.json" ]; then
     exit 1
 fi
 
+IMAGE_NAME=${NAME}
 if [[ -z "${IMAGE_NAME}" ]]; then
     IMAGE_NAME=${GITHUB_REPOSITORY}
 fi
 
-if [[ ! -z "${IMAGE_TAG}" ]]; then
+IMAGE_TAG=${TAG}
+if [[ -z "${IMAGE_TAG}" ]]; then
     IMAGE_TAG=${GITHUB_REF#*/}
     IMAGE_TAG=${IMAGE_TAG#*/}
     IMAGE_TAG=$(echo $IMAGE_TAG | sed -e "s#^v##")
-    
+
     if [ "$IMAGE_TAG" == "master" ]; then
         IMAGE_TAG=latest
     fi
-else
-    IMAGE_TAG=latest
 fi
 
 echo ::set-env name=IMAGE_TAG::${IMAGE_TAG}
